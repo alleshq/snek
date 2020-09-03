46e3d4adbc2c2deb6f3bd044d21c88080e9837ce
Archie Baer <archie@abaer.dev>
2020-09-03 23:44:03 +0100

    Session token cookie

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 require("dotenv").config();
 
+const jwt = require("jsonwebtoken");
+
 // web server and stuff
 const express = require("express");
 const app = express();
+app.use(require("cookie-parser")());
+app.use((_err, _req, res, _next) => res.status(500).send("oh fuck it broke"));
 app.listen(8080, () => console.log("server do be listening"));
 
 // who tf r u
@@ -12,10 +16,15 @@ app.get("/auth/cb", (req, res) => {
     const {token} = req.query;
     if (typeof token !== "string") return res.status(400).send("gimme token pls");
 
-    // get teh usr id from token
+    // get teh usr id from quickauth token
     quickauth(token, process.env.QUICKAUTH_CALLBACK)
         .then(id => {
-            res.send(`u r user ${id}`);
+            // sign dat session token
+            const token = jwt.sign({user: id}, process.env.SESSION_SECRET);
+
+            // set yummy cookie
+            res.cookie("token", token);
+            res.redirect("/");
         })
         .catch(() => res.status(401).send("dat not real token"));
 });
