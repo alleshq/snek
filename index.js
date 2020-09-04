919245a3287d78f4aec08f1aad806b9a942aba31
Archie Baer <archie@abaer.dev>
2020-09-04 12:38:34 +0100

    Slower speed

--- index.js
+++ index.js
@@ -82,7 +82,7 @@ setInterval(() => {
     // move sneks
     Object.keys(players).forEach(id => {
         const p = players[id];
-        if (ticks % (Math.floor(p.segments.length / 10) + 1) === 0) {
+        if (ticks % (Math.floor(p.segments.length / 10) + 2) === 0) {
             const {x: oldX, y: oldY} = p.segments[0];
             p.segments.unshift({
                 x: p.direction === "left" ? oldX + 1 : p.direction === "right" ? oldX - 1 : oldX,
