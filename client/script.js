835240cdec4e76686af7ed9589f92524ec35576c
Archie Baer <archie@abaer.dev>
2020-09-04 01:19:47 +0100

    Canvas rendering

--- client/script.js
+++ client/script.js
@@ -1,9 +1,38 @@
+let user;
+
 fetch("/user").then(async res => {
     if (res.status === 200) {
-        const user = await res.json();
+        user = await res.json();
         document.querySelector(".signedIn span").innerText = user.nickname;
         document.querySelector(".signedIn").style.display = "block";
     } else {
         document.querySelector(".signedOut").style.display = "block";
     }
 });
+
+const setScreen = playing => {
+    document.querySelector("main").style.display = playing ? "none" : "block";
+    document.querySelector("canvas").style.display = playing ? "block" : "none";
+};
+
+document.querySelector("button").onclick = () => {
+    setScreen(true);
+};
+
+const gameSize = Math.min(innerWidth, innerHeight);
+const tileSize = gameSize / 20;
+const canvas = document.querySelector("canvas");
+canvas.width = gameSize;
+canvas.height = gameSize;
+const c = canvas.getContext("2d");
+c.fillStyle = "black";
+
+const render = tiles => {
+    c.clearRect(0, 0, gameSize, gameSize);
+    for (let x = 0; x < 20; x++) {
+        for (let y = 0; y < 20; y++) {
+            c.fillStyle = tiles[x][y];
+            c.fillRect(tileSize * x, tileSize * y, tileSize, tileSize);
+        }
+    }
+};
